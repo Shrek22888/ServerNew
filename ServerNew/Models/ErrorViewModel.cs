714933ba2f3d66532565b40dbe3a99e5207c6b2b
NikitaLyubimov <sobachiy.deputat2017@yandex.ru>
2020-03-14 00:34:19 +0300

    first commit

---
+++ ServerNew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServerNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
